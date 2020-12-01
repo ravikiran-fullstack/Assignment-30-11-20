1f2b5eb6ede04fbe257bd2a6101efa2e3953ab7b
kiran-code <ravikiransjce.code@gmail.com>
2020-12-01 20:59:49 +0530

    implemented callback approach for countdown timer

--- app.js
+++ app.js
@@ -1,32 +1,71 @@
-function startCountDown(){
-  showMessage();
-}
+
 //Configure
 let timeoutID;
 let count = 10;
 
-function showMessage() {
-    timeoutID = setTimeout(() =>{
-      document.getElementById('count').innerHTML = `Count Down Started: ${count}`;
+// Function uses recursion 
+function showMessage1() {
+  timeoutID = setTimeout(
+    () => {
+      document.getElementById("count1").innerHTML = `Count Down Started: ${count} [Using recursion]`;
       count--;
-      if(count !== 0){
-        timer(showMessage); // callback function called, which is passed as argument
-      }else {
-        document.getElementById('count').innerHTML = '';
-        document.getElementById('info').innerHTML = 'Thank you for your patience, welcome to the page';
+      if (count !== -1) {
+        timer(showMessage1); // timer callback function called, which is passed as argument
+      } else {
+        document.getElementById("count1").innerHTML = "";
+        document.getElementById("info1").innerHTML =  "Thank you for your patience, welcome to the page";
         clearTimer();
       }
-    }
-    , 1000, timer); // Callback function passed as argument to the setTimeout function
+    },1000,timer); // timer(callback) function passed as argument to the setTimeout function
 }
 
-function timer(callback){
+function timer(callback) {
   console.log(callback);
-  setTimeout(()=> {
+  setTimeout(() => {
       callback();
-  },0, callback);
+    },0,callback);
 }
 
 function clearTimer() {
-    clearTimeout(timeoutID);
+  clearTimeout(timeoutID);
 }
+
+
+//Function uses callbacks 
+function showMessage2(){
+  setTimeout(()=> {
+    document.getElementById("count2").innerHTML = `Count Down Started: 10 [Using callbacks]`;
+    setTimeout(() => {
+      document.getElementById("count2").innerHTML = `Count Down Started: 9 [Using callbacks]`;
+      setTimeout(()=> {
+        document.getElementById("count2").innerHTML = `Count Down Started: 8  [Using callbacks]`;
+        setTimeout(() => {
+          document.getElementById("count2").innerHTML = `Count Down Started: 7 [Using callbacks]`;
+          setTimeout(()=> {
+            document.getElementById("count2").innerHTML = `Count Down Started: 6 [Using callbacks]`;
+            setTimeout(() => {
+              document.getElementById("count2").innerHTML = `Count Down Started: 5 [Using callbacks]`;
+              setTimeout(()=> {
+                document.getElementById("count2").innerHTML = `Count Down Started: 4 [Using callbacks]`;
+                setTimeout(() => {
+                  document.getElementById("count2").innerHTML = `Count Down Started: 3 [Using callbacks]`;
+                  setTimeout(()=> {
+                    document.getElementById("count2").innerHTML = `Count Down Started: 2 [Using callbacks]`;
+                    setTimeout(() => {
+                      document.getElementById("count2").innerHTML = `Count Down Started: 1 [Using callbacks]`;
+                      setTimeout(()=> {
+                        document.getElementById("count2").innerHTML = "";
+                        document.getElementById("info2").innerHTML ="Thank you for your patience, welcome to the page";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}
+
--- dom.js
+++ dom.js
@@ -1,8 +1,14 @@
 const div = document.createElement('div');
 const p1 = document.createElement('p');
-p1.id = 'count';
+p1.id = 'count1';
 
 const p2 = document.createElement('p');
-p2.id = 'info';
-div.append(p1,p2)
+p2.id = 'count2';
+
+const p3 = document.createElement('p');
+p3.id = 'info1';
+const p4 = document.createElement('p');
+p4.id = 'info2';
+
+div.append(p1, p2, p3, p4)
 document.body.append(div);
